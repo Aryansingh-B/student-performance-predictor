0b90d41880c90bb22acb9e4a00fcef3742ed916d
Aryansingh <baisaryansingh@gmail.com>
2026-06-13 15:46:22 +0530

    Remove duplicate startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f "model/model.pkl" ]; then
-    echo "Model not found. Training now..."
-    python data/generate_data.py
-    python -m src.train
-fi
-streamlit run app/streamlit_app.py
